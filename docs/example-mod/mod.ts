a635ba4f3e50ec94d8e376b6722a53393d5bfd1d
Orie Steele <orie@transmute.industries>
2021-01-17 11:33:07 -0600

    Add example

--- docs/example-mod/mod.ts
+++ docs/example-mod/mod.ts
@@ -1,3 +1,8 @@
+// DENO DID PM Module as:
+// http://localhost:8000/did-modules/did:web:or13.github.io:deno-did-pm?service=github&relativeRef=/OR13/deno-did-pm/master/docs/example-mod/mod.ts
+// GitHub URL as:
+// https://raw.githubusercontent.com/OR13/deno-did-pm/master/docs/example-mod/mod.ts
+
 export const sayHello = () => {
   console.info("hello");
 };
